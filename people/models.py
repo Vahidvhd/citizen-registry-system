f0fc6ecf7c329d1d6595172241fec9800e0dd5fa
Vahidvhd <vahidvhd91@gmail.com>
2026-06-06 21:29:19 +0100

    Add citizen model, tests, and PostgreSQL setup

--- people/models.py
+++ people/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Citizen(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    national_code = models.CharField(max_length=10, unique=True)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    address = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.national_code}'
--- people/tests.py
+++ people/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Citizen
 
 # Create your tests here.
+class PeopleAppSmokeTest(TestCase):
+    def test_peaople_app_is_working(self):
+        self.assertTrue(True)
+
+class CitizenModelTest(TestCase):
+    def test_citizen_string(self):
+        citizen= Citizen.objects.create(
+            first_name ='vahid',
+            last_name='vahedi',
+            national_code='1234567890',
+            date_of_birth='1991-01-01',
+            phone_number='07415678901',
+            email='vahid@test.com',
+            address='London',
+        )
+        self.assertEqual(str(citizen), 'vahid vahedi - 1234567890')
+
