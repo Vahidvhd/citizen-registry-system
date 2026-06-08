1250afa250fbfddd9110106431a341f2ba59d700
Vahidvhd <vahidvhd91@gmail.com>
2026-06-08 19:51:03 +0100

    Add signup and login views with tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignupViewTest(TestCase):
+    def test_signup_page_returns_200(self):
+        response = self.client.get(reverse('signup'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+
+    def test_user_can_signup_with_valid_data(self):
+        response = self.client.post(
+            reverse('signup'),
+            data={
+                'username': 'vahid',
+                'password1': 'testpassword!',
+                'password2': 'testpassword!',
+            }
+        )
+
+        self.assertEqual(User.objects.count(), 1)
+        self.assertRedirects(response, reverse('login'))
---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import SignUpView
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view() , name='signup'),
+    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+
+]
