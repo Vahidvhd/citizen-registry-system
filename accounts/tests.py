97b778fb1c79aa9582434d17ada77be80c2f3ba1
Vahidvhd <vahidvhd91@gmail.com>
2026-06-09 12:43:17 +0100

    Add login and logout views with tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -15,10 +15,36 @@ class SignupViewTest(TestCase):
             reverse('signup'),
             data={
                 'username': 'vahid',
-                'password1': 'testpassword!',
-                'password2': 'testpassword!',
+                'password1': 'Testpassword!',
+                'password2': 'Testpassword!',
             }
         )
 
         self.assertEqual(User.objects.count(), 1)
         self.assertRedirects(response, reverse('login'))
+
+
+class LoginViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='vahid',
+            password='Testpassword!',
+        )
+
+    def test_login_page_returns_200(self):
+        response = self.client.get(reverse('login'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    def test_user_can_login_with_valid_credentials(self):
+        response = self.client.post(
+            reverse('login'),
+            data={
+                'username': 'vahid',
+                'password': 'Testpassword!',
+            }
+        )
+
+        self.assertRedirects(response, reverse('signup'))
--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'signup'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
