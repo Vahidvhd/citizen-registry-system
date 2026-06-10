92e888ef9e0064973cff9b4ee42dae48d492225b
Vahidvhd <vahidvhd91@gmail.com>
2026-06-10 14:28:31 +0100

    Add usage profile model and tests

--- usage/models.py
+++ usage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class UsageProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    daily_limit = models.PositiveIntegerField(default=10)
+    monthly_limit = models.PositiveIntegerField(default=100)
+    daily_used = models.PositiveIntegerField(default=0)
+    monthly_used = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user.username} usage profile'
--- usage/tests.py
+++ usage/tests.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from .models import UsageProfile
 
-# Create your tests here.
+
+class UsageProfileModelTest(TestCase):
+    def test_usage_profile_string_representation(self):
+        user = User.objects.create_user(
+            username='vahid',
+            password='Vahidtest!',
+        )
+
+        profile = UsageProfile.objects.create(
+            user=user,
+        )
+
+        self.assertEqual(
+            str(profile),
+            'vahid usage profile'
+        )
+
+    def test_usage_profile_default_limits(self):
+        user = User.objects.create_user(
+            username='vahid',
+            password='Vahidtest!',
+        )
+
+        profile = UsageProfile.objects.create(
+            user=user,
+        )
+
+        self.assertEqual(profile.daily_limit, 10)
+        self.assertEqual(profile.monthly_limit, 100)
+        self.assertEqual(profile.daily_used, 0)
+        self.assertEqual(profile.monthly_used, 0)
